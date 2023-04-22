b947b5c0761270718029d9c3ef47f44fb8d25561
Shifadewani2209 <113427997+Shifadewani2209@users.noreply.github.com>
2023-04-22 19:51:27 +0530

    Summary

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+int main(){
+    map<string,int>marksMap;
+    marksMap["shifa"] = 99;
+    marksMap["rohan"] = 40;
+    marksMap["sammyyy"] = 68;
+    return 0;
+}
